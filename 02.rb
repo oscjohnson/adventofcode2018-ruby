54a1614a02e2da5d1f48d8ff081c8b063116d711
Oscar Johnson <o.johnsson@live.se>
2018-12-02 16:13:28 +0100

    Day 2 completed

---
+++ 02.rb
@@ -0,0 +1,298 @@
+lines = (ARGV.empty? ? DATA : ARGF).readlines.map(&:chomp)
+
+def count_letters(str)
+	map = Hash.new(0)
+	str.split("").each {|c|
+		map[c]+=1
+	}
+	map
+end
+
+part1 = lines.inject(Hash.new(0)) { |acc, s|
+	map = count_letters(s).select {|k,v| v > 1}
+
+	[2,3].each { |i|
+		acc[i] += 1 if map.values.include? i
+	}
+
+	acc
+}.values.inject(1) { |acc, item| acc * item}
+
+puts part1
+
+def remove_char_at(str, idx)
+	return str.split("").dup.tap{|i| i.delete_at(idx)}.join("") 
+end
+
+found = false
+part2 = ""
+lines.each_with_index { |s, i|
+	lines.each_with_index { |compare_to, j|
+		next if i == j
+
+		compare_to.length.times { |k|
+			if remove_char_at(s, k) == remove_char_at(compare_to, k)
+				part2 = remove_char_at(s, k)
+				found = true
+				break
+			end
+		}
+
+		break if found
+	}
+	break if found
+}
+
+puts part2
+
+__END__
+bazvmqthjtrnlosiecxyghkwud
+pazvmqbijirzlosiecxyghkwud
+pazvtqbmjtrnlosiecxyghkwzd
+pazvmqbfjtrjlosnlcxyghkwud
+pazvkqbfjtrtlosiecjyghkwud
+paztmqbfjtrnbosiecxyglkwud
+pazvmqbfjtunlosievxmghkwud
+pazvmqbfjtmngosiecyyghkwud
+jazvmqbfjtrnlosiecxygikpud
+pazvqqbfctrnlosimcxyghkwud
+pazvmqbfjtrnwogiecxyihkwud
+pazvmqbfjtrqlojiecxeghkwud
+payvmqbfjtrzlosiecxyghkwuk
+pkzvmqnfjtrnlosieciyghkwud
+pazvmqqfjtrnldsiecxyghkwui
+pazvmqbfttrqlosiecxywhkwud
+gazvmybfjthnlosiecxyghkwud
+pazvmqbfjtrnlasiecxygptwud
+pktvmqbfjtrnwosiecxyghkwud
+pazvmqwfjtrnlosiecxgghkkud
+pazvmzkbjtrnlosiecxyghkwud
+pazvmqbfjtrnloslecxyghuwui
+pezvmqbfjtrnlesieyxyghkwud
+cazvmqbfjrrnlosiecxyghkmud
+pazvmqrfjjrnlosiecxyghkwnd
+pazvmqbgjtrnlosiecxyphtwud
+pazvmqbvmtrnlosiecxyghkpud
+pazdmqbfjtrnlosiecxyuhkpud
+pazvmqbflnrnloshecxyghkwud
+pazvvqbfjprilosiecxyghkwud
+pazvwqbfjtrllosiecxyghknud
+pazvmqbfjtrnloniecxdghkaud
+pazvmqbfjtrnlvsuecxynhkwud
+ptzvmqwfjtrnlosieccyghkwud
+pkzvmqbjjtrnlosiecryghkwud
+pazvmqqfjtrexosiecxyghkwud
+pazgmqbfjtrneoyiecxyghkwud
+paznmqbfjtrnlosiecxydhkwzd
+pazvmqbfjtrnaosiwcxsghkwud
+pazomqbfjxrnlosiewxyghkwud
+pazsmqbfjprnlosiecxrghkwud
+pazvmqbfqtrnoosiecxygmkwud
+aazvmqbfjtrnlosiacxyghjwud
+pazviqbfjtrnlobiecxygrkwud
+qazwmqbfjhrnlosiecxyghkwud
+pazvmqbfftrnlosiqcxygfkwud
+patvmqbfjtonlosircxyghkwud
+pazvmqbfjtrnlomaecxyghkpud
+paztmqbfjtrulossecxyghkwud
+pazvmqbijtrnlobiecxyghkwkd
+pazvsqbfjtrnlospecxyghkqud
+pbzmmqbfjtrnlosiecxyghkwhd
+pezvkqbfjtenlosiecxyghkwud
+razvmqbfjkrnlosiecxeghkwud
+pazcmqbfjtrnloriecxyghkgud
+pazvmqbfftfnlosiecvyghkwud
+pazvmqpujtrnlosiepxyghkwud
+patvgqbfjtrnloslecxyghkwud
+pazvmqbfltrnlosibcxyghswud
+pazvmebfjtrnlosaecxyehkwud
+pazdmqbejtrnlosiecxyghrwud
+pazvmcbfntrplosiecxyghkwud
+pszvmqbfjtrnlosivcfyghkwud
+puzvmqbfjtrnloeiecxyxhkwud
+pazvmqbfjtrivooiecxyghkwud
+pazvyqbfjtrngosiwcxyghkwud
+pauvmqbfjtrnlosimexyghkwud
+pazvmqbfjtrnwoshecxeghkwud
+dazvmqbfjtrnloshecxygxkwud
+pazvmqbfjtrtdosiecxyghvwud
+pazxmqbfjtrnlosieceyghjwud
+pazvmqbfjtrnlosihexjghkwud
+pazvmqbfjsrnlosiecxughiwud
+phzvcqbfjtrqlosiecxyghkwud
+pazvmibfjtrnlosjecxxghkwud
+pazvmqbfjtrbeosiecxlghkwud
+pazvmqyfjttolosiecxyghkwud
+fawvmqbfjtrnlosiecxyghkwhd
+pazvmqbfjprnxosiecxyghkbud
+macvmqbfjtrnlosiesxyghkwud
+pazsmqbfjtrflouiecxyghkwud
+pacvmqbfjtrnltsiecxyghcwud
+pazvmqbfjtymlosiecxygykwud
+pazvmqbfjtrclosiecxygukwmd
+pazvmqbfjtrnlobiecxphhkwud
+mazvmqbhitrnlosiecxyghkwud
+pazvmqdtjtrnlrsiecxyghkwud
+pazvmqbfjgrnllsieczyghkwud
+pazvmqbfjtrilosiecxxgikwud
+pazvmqbjjtrnlosreceyghkwud
+paxvmmbfjtrilosiecxyghkwud
+pazqmwbfjtrnlowiecxyghkwud
+pazvmqbfjfrnqosiecxyghkwui
+pazvmqbfjtrrgosiecxyghswud
+pazvmqnfjtrnlosiecsyghkwmd
+paiemqbmjtrnlosiecxyghkwud
+pazvmqbfdtqnlosiecxyjhkwud
+pazvmxbfjthndosiecxyghkwud
+pqzvmqbfjtrnlosiecxbghkzud
+pagrmqbfjtrnlosiecxygskwud
+pazamqtfjtrnsosiecxyghkwud
+pazvmqbfjtrnldshecxyzhkwud
+pazvmnbfjtrllosieclyghkwud
+snzvmqbfjnrnlosiecxyghkwud
+pazvsqbfjdrnlosiecxyghswud
+pazvmqnfjfrnlosiecsyghkwud
+pazvmqbfjtrnlosiecxjghowum
+pazvmqbfjtjnlosieczygfkwud
+pazvmqbsjtrnloziecxyghkeud
+pazvxqbgjtrnlooiecxyghkwud
+pazvmqbfjtrnlooiecxmyhkwud
+pazvmqbyftrnlosgecxyghkwud
+pazvmqbfjtrnlosiwcxyqhksud
+pazvmqkyjtrnlokiecxyghkwud
+pazfmqbfjtrnlosijcxyohkwud
+pazvmqbfjtrnlociecxygikcud
+fazvmqbfjtrnlosiecxyjhkuud
+pazvmqbojtknlohiecxyghkwud
+pazvmqbfjtgnlosbecxyghkwux
+pazvmqbfjtrnlocieckoghkwud
+pazvdqbfjtrlltsiecxyghkwud
+pazvmqbfjtsnlfsiecxyglkwud
+przvpqbfjtrnyosiecxyghkwud
+pazvmbrfjtrnlosiecxmghkwud
+dazvmqbfttrnlostecxyghkwud
+pazvmqbfttdnlosiecxygwkwud
+pazvmqbvitrnlosieexyghkwud
+pazvmqbfjhrnlosjecxyvhkwud
+pazvmqbfstrnlosiecxyggkwpd
+bazvmqbfjtrnlmsiecxyohkwud
+patmmqbfjtrnlosizcxyghkwud
+pazvmqbfwtrglosieqxyghkwud
+pazvmqbfjtrnlosiecxdhhkwmd
+pazvmqbfjdrnlosnexxyghkwud
+oazrrqbfjtrnlosiecxyghkwud
+pazvmqbfjcrnlosiecxygakwjd
+pazvmqbfjtrnlosifcxfghkwyd
+pazvmnbfjtrnlosiecxyahzwud
+pazvmqbfgtrnlojiecxyghkgud
+pazvmqbfjtrnlaliecxyghkwuy
+pazvmqbfjtrnlfsiecrtghkwud
+pazvmqbkjtrnloswecxdghkwud
+pazvtqbfjtdnlosiecxyghkwuu
+pozvmqbfrtrnlosiesxyghkwud
+payvmqbfjornlossecxyghkwud
+pazvuqbfjtrnlosiscxyghkpud
+pgzcmqbfjtrnlotiecxyghkwud
+pazvvqbfjtrnlobieyxyghkwud
+pazycqbfjtrnlosiecxyzhkwud
+pizvdqbfjtrnlosiecxbghkwud
+pazvmqbfjtrnloqiecxmgtkwud
+gazvmqbfjtrnlusiecxpghkwud
+pazvmqdfjtralosiecxyghkwmd
+pazvmqbfjtmnlosiecxywhawud
+pazvlqbfjtrnlosqecxyghqwud
+pazvmqbfjtrnlhsneixyghkwud
+kazvmqbfjtrqlosimcxyghkwud
+pazvmwbfjtrclosiecxyghkuud
+pazvmqjfjtrnlosieckyghpwud
+pezvmqbgjtrnloseecxyghkwud
+pazvqqbfjtfnlosvecxyghkwud
+oazvmqbfjtunlosiecxyghkwad
+pazvmqbfjtrncoswecxyghfwud
+pazvyqbfjtrnlosqecxygtkwud
+pazvmqbfjtrvlzsiecxygwkwud
+pazvmqbfjjrnlosiekxylhkwud
+madvmqbfjtrnlosircxyghkwud
+pazvmybfjtrnlisiecxyghkwbd
+pazvmqbjjixnlosiecxyghkwud
+pazvmqefjtrnloqiecxyghhwud
+pazveqbfjtrnlosiecgygzkwud
+pazvmqbfjtrxlosiecxmgwkwud
+uazvmqufjtrnlosiecxyghkwuo
+pasymqbfjtrnlosiecxyghowud
+pazvmqbfjtlnlpsiecxyghswud
+pnzvmqbfjprnloszecxyghkwud
+pafjmqcfjtrnlosiecxyghkwud
+pazvmqxfbtrnloqiecxyghkwud
+pazvmzbfjtrnposiccxyghkwud
+pazvmqbfjotulosiecxyghkwud
+pazvmqbfotrnlosgecxykhkwud
+przvmqbfjtrnlosiecxyqhkwcd
+pazvmqbfjtsnlogiecxyyhkwud
+pazvmqbfrtrnlzsiecxyghkwug
+pazvmqbfjtrnlosiecxzgukwuo
+pqzvmqbqjtrnlosdecxyghkwud
+pazvmqbfjtqqlosiecxughkwud
+pazvmqbfjtrnlosiedhyphkwud
+pazsmqbcutrnlosiecxyghkwud
+pazvmqbgrtrnlosiecxyghpwud
+pazemqbfjtznlosiecxyghkvud
+pazvkqbfjtrilosiecxyghkwod
+pfzvmqbfjtrnlopiecxygjkwud
+pazvmqvfjtreloniecxyghkwud
+pazvmqbfjernljsiecxgghkwud
+pazvmqikjtrnlosiecxyghqwud
+pazvmqbfjtrnpesoecxyghkwud
+fazvmqbfjtrnlosihchyghkwud
+pazvmqbfjtgnloanecxyghkwud
+pazvmqsfjqrnlosiecxychkwud
+parvmqbfjtrnlosiecxygfuwud
+przvmqbfjtrhlosihcxyghkwud
+pazvmqbcjtrnlosimcxgghkwud
+pazvmqbfjtrnlosceciyjhkwud
+pazvkqbfjtrylosivcxyghkwud
+pazvmqbfjtrnlgsieoxyghdwud
+pazvmqnfstrnlowiecxyghkwud
+pazvmqbfdtrnlosieumyghkwud
+pazvmqbfjtrnlosyecxfghkwul
+pazvmqbfjtrclosivcxyghkcud
+pazjmqbfjtrnlosiecxygokwkd
+hazvmqbfjtrflosiecxzghkwud
+wazvmqbfjtrnlomiecxyphkwud
+yazvmqbfjirnkosiecxyghkwud
+pczvmqbfjtrnlohiecxyghkwpd
+pazvmqbfotrbeosiecxlghkwud
+pazvmqbfjtrplosiecxynhzwud
+paxvbqbwjtrnlosiecxyghkwud
+pazvmqvfjtrnlosiecbyghqwud
+pazjmqbfjtrnlosiecxoghkwed
+pazvmqbfjtreljsitcxyghkwud
+mazamqbfjtrnlosiecxoghkwud
+pazvmqbfjjrnposiscxyghkwud
+pbrvmqbfjtrnloliecxyghkwud
+pazvmqbfjtrnlosiecxgghkyyd
+pmzvmqbfntrnlosiecxyghkwuw
+pazvzqbfjtrnlosienxyghzwud
+pazvmqifjtvnlosrecxyghkwud
+tazvmqbhjtjnlosiecxyghkwud
+pazvmqbfjtlnxosiecxyghkwuo
+pazvmqbfjennlosiecxyghkwxd
+pahvmqbfjhrnlosiecxythkwud
+pazvmlkfjtrnlxsiecxyghkwud
+pfzvmqbojtrnlosieciyghkwud
+pazvbqbfjtrollsiecxyghkwud
+eazvmqbfjtrnlosiecayghkoud
+pazvmqbfjtjnlvsiecxyghkwsd
+pazvoqbojtrnlosiecfyghkwud
+pazvmqbfjtuslosiecxyghksud
+pazvmqbfjnrnlosiedxyghkwup
+pazvmqbjjtrnlosieaxyghdwud
+pazccqbfjtrhlosiecxyghkwud
+pbzvmqkfjtrnlosievxyghkwud
+pazvmqrljtrnlosiscxyghkwud
+pazvmqbfjfoqlosiecxyghkwud
+pazcmqbfjtrnlosiecxyihkwuf
+pszvmqbfjtrnnosiacxyghkwud
+aazvmqbfjtrnlosieyxyghkwld
+pazvrqbfntrnlosiycxyghkwud
+pkzvoqbfjtrnlosiecxyghxwud
